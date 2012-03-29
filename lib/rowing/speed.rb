4daca34f01fbeaae6d28e5f7174f94fdca7be3a0
John Maxwell <jgwmaxwell@gmail.com>
2012-03-29 13:17:54 +0100

    fixed parse_time as class method

--- lib/rowing/speed.rb
+++ lib/rowing/speed.rb
@@ -6,21 +6,22 @@ module Rowing
     # This function takes a properly formatted time string, and turns
     # it into a float of seconds elapsed.
     # e.g. "6:14.2" -> 374.2
-  
-    def parse_time(time = "0:00.0")
-      comp = time.split(":")
-      hours, mins, secs = 0, 0, 0.0
-      case comp.size
-      when 2
-        mins = comp[0].to_i
-        secs = comp[1].to_f
-      when 3
-        hours = comp[0].to_i
-        mins = comp[1].to_i
-        secs = comp[2].to_f
+    class << self
+      def parse_time(time = "0:00.0")
+        comp = time.split(":")
+        hours, mins, secs = 0, 0, 0.0
+        case comp.size
+        when 2
+          mins = comp[0].to_i
+          secs = comp[1].to_f
+        when 3
+          hours = comp[0].to_i
+          mins = comp[1].to_i
+          secs = comp[2].to_f
+        end
+        time = hours * 3600 + mins * 60 + secs
+        time
       end
-      time = hours * 3600 + mins * 60 + secs
-      time
     end
   end
 end
